2d2f2605f3fc8ac96f537b42d6a800d07b7addfb
Straltsou Maxim <green1971weekend1@gmail.com>
2020-10-25 02:34:49 +0300

    feat: initial commit plus header added

---
+++ src/components/Feed.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import logo from '../logo.svg'
+
+class Feed extends React.Component {
+    
+}
+
+
+export default Feed;
---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <div className="container h-flex">
+                    <a href="#" className="logo">
+                        <img src={logo} alt="logo"/>
+                    </a>
+                    <nav className="links">
+                        <ul>
+                            <li>
+                                <a href="#" className="menu__links">Content</a>
+                            </li>
+                            <li>
+                                <a href="#" className="menu__links">Profile</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        )
+    }
+}
+
+
+export default Header;
